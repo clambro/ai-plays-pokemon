8238dae3b78e3b246d5aa7a21bd6c2a2379a8463
clambro <clambro.git@gmail.com>
2025-05-01 20:03:24 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import argparse
+import asyncio
+from pathlib import Path
+
+from emulator.context import EmulatorContext
+
+
+async def main(rom_path: str, state_path: str | None = None) -> None:
+    """
+    Get the emulator ticking on an async thread, and iteratively run the agent.
+
+    :param rom_path: The path to the ROM file.
+    :param state_path: Optional path to load a saved state from.
+    """
+    async with EmulatorContext(rom_path, state_path):
+        await asyncio.sleep(10)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rom-path", type=str, required=True)
+    parser.add_argument("--state-path", type=str, required=False)
+    args = parser.parse_args()
+    asyncio.run(main(args.rom_path, args.state_path))
