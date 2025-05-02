55573da83d515e1b4c4e25bb3d92188e3c79e68d
clambro <clambro.git@gmail.com>
2025-05-01 18:58:15 -0700

    emulator stuff

---
+++ emulator/emulator.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+from pyboy import PyBoy
+from emulator.game_state import YellowLegacyGameState
+
+
+class YellowLegacyWrapper:
+    """
+    Wrapper for accessing the game state of Pokemon Yellow Legacy. Encapsulates the PyBoy API so
+    that the rest of the codebase doesn't need to worry about emulation or memory addresses.
+    """
+
+    def __init__(self, rom_path: str, initial_state_path: str | None = None) -> None:
+        self.tick_num = 0
+        self.pyboy = PyBoy(rom_path)
+        if initial_state_path:
+            with Path(initial_state_path).open("rb") as f:
+                self.pyboy.load_state(f)
+
+        self._game_state = YellowLegacyGameState.from_memory(self.pyboy.memory, self.tick_num)
+
+    @property
+    def game_state(self) -> YellowLegacyGameState:
+        """
+        Get the current game state, lazily updating it if necessary.
+
+        :return: The current game state.
+        """
+        if self.tick_num != self._game_state.tick_num:
+            self._game_state = YellowLegacyGameState.from_memory(self.pyboy.memory, self.tick_num)
+        return self._game_state
+
+    def tick(self, count: int = 1) -> bool:
+        """
+        Tick the emulator forward by `count` frames.
+
+        :param count: Number of frames to tick forward.
+        :return: Whether the game is still running.
+        """
+        self.tick_num += count
+        return self.pyboy.tick(count, render=True, sound=True)
+
+    def stop(self) -> None:
+        """Stop the emulator."""
+        self.pyboy.stop()
---
+++ emulator/game_state.py
@@ -0,0 +1,91 @@
+from typing import Self
+
+import numpy as np
+from constants import (
+    CUT_TREE_TILE,
+    PIKACHU_TILE,
+    SPRITE_TILE,
+    WALL_TILE,
+    WARP_TILE,
+    WATER_TILE,
+    GRASS_TILE,
+    LEDGE_TILE,
+    FREE_TILE,
+    PLAYER_TILE,
+    PLAYER_OFFSET_Y,
+    PLAYER_OFFSET_X,
+)
+from emulator.schemas import MapState, PlayerState, ScreenState, BattleState
+
+
+from pyboy import PyBoyMemoryView
+from pydantic import BaseModel
+
+
+class YellowLegacyGameState(BaseModel):
+    """A snapshot of the Pokemon Yellow Legacy game state."""
+
+    tick_num: int
+
+    player: PlayerState
+    cur_map: MapState
+    screen: ScreenState
+    battle: BattleState
+
+    @classmethod
+    def from_memory(cls, mem: PyBoyMemoryView, tick_num: int) -> Self:
+        """
+        Create a new game state from a snapshot of the memory.
+
+        :param mem: The PyBoyMemoryView instance to create the game state from.
+        :return: A new game state.
+        """
+        return cls(
+            tick_num=tick_num,
+            player=PlayerState.from_memory(mem),
+            cur_map=MapState.from_memory(mem),
+            screen=ScreenState.from_memory(mem),
+            battle=BattleState.from_memory(mem),
+        )
+
+    def get_ascii_screen(self) -> list[list[str]]:
+        """Get an ASCII representation of the current screen."""
+        tiles = np.array(self.screen.tiles)
+        # Each block on screen is a 2x2 square of tiles.
+        blocks = []
+        for i in range(0, tiles.shape[0], 2):
+            row = []
+            for j in range(0, tiles.shape[1], 2):
+                b = tiles[i : i + 2, j : j + 2]
+                if np.any(b == self.cur_map.water_tile):
+                    row.append(WATER_TILE)
+                elif np.isin(b, self.cur_map.ledge_tiles).any():
+                    row.append(LEDGE_TILE)
+                elif np.any(b == self.cur_map.grass_tile):
+                    row.append(GRASS_TILE)
+                elif b.flatten().tolist() == self.cur_map.cut_tree_tiles:
+                    row.append(CUT_TREE_TILE)
+                elif not np.isin(b, self.cur_map.walkable_tiles).any():
+                    row.append(WALL_TILE)
+                else:
+                    row.append(FREE_TILE)
+            blocks.append(row)
+        blocks = np.array(blocks)
+
+        blocks[PLAYER_OFFSET_Y, PLAYER_OFFSET_X] = PLAYER_TILE
+        for s in self.cur_map.sprites:
+            if not s.is_rendered:
+                continue
+            if screen_coords := self.screen.get_screen_coords(s.y, s.x):
+                blocks[screen_coords[0], screen_coords[1]] = SPRITE_TILE
+
+        pikachu = self.cur_map.pikachu_sprite
+        if pikachu.is_rendered:
+            if screen_coords := self.screen.get_screen_coords(pikachu.y, pikachu.x):
+                blocks[screen_coords[0], screen_coords[1]] = PIKACHU_TILE
+
+        for w in self.cur_map.warps:
+            if screen_coords := self.screen.get_screen_coords(w.y, w.x):
+                blocks[screen_coords[0], screen_coords[1]] = WARP_TILE
+
+        return blocks.tolist()
