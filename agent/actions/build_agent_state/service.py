50c6203f9981567d224bfa62ae0381e62f25ea7c
clambro <clambro.git@gmail.com>
2025-05-04 17:23:46 -0700

    bugfix

--- agent/actions/build_agent_state/service.py
+++ agent/actions/build_agent_state/service.py
@@ -11,10 +11,21 @@ class BuildAgentStateService:
         self.emulator = emulator
 
     async def wait_for_movement_end(self) -> None:
-        """Check if the player is moving, and if so, wait for them to stop."""
+        """
+        Check if the player is moving, and if so, wait for them to stop.
+
+        This is to ensure that the player is not moving when the agent loop starts. We need to run
+        the check a few times to ensure that we didn't just catch the player between animation
+        frames.
+        """
+        successes = 0
         while True:
             game_state = self.emulator.get_game_state()
-            if not game_state.is_player_moving:
+            if game_state.is_player_moving:
+                successes = 0
+                logger.info("Player movement detected. Waiting for it to end.")
+            else:
+                successes += 1
+            if successes >= 3:
                 break
-            logger.info("Player is still moving. Waiting before initiating the agent loop.")
-            await asyncio.sleep(0.1)
+            await asyncio.sleep(0.01)
