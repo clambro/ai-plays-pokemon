6d91bdb9bc94c2a0c085cb76e311f493e97baf2c
clambro <clambro.git@gmail.com>
2025-05-05 19:46:59 -0700

    change folder structure

--- agent/state.py
+++ agent/state.py
@@ -11,8 +11,7 @@ from raw_memory.schemas import RawMemory
 class AgentState(BaseModel):
     """The state used in the agent graph workflow."""
 
-    memory_dir: Path
-    backup_dir: Path
+    folder: Path
     iteration: int = 0
     buttons_pressed: list[Button] = Field(default_factory=list)
     raw_memory: RawMemory = Field(default_factory=RawMemory)
@@ -22,8 +21,7 @@ class AgentState(BaseModel):
 class AgentStateParams(StrEnum):
     """The parameters for the agent state as strings for use with Burr."""
 
-    memory_dir = "memory_dir"
-    backup_dir = "backup_dir"
+    folder = "folder"
     iteration = "iteration"
     buttons_pressed = "buttons_pressed"
     raw_memory = "raw_memory"
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import asyncio
+from datetime import datetime
 from loguru import logger
 from pathlib import Path
 from agent.app import build_agent_application
@@ -10,8 +11,7 @@ import aiofiles
 
 async def main(
     rom_path: str,
-    memory_dir: str,
-    backup_dir: str,
+    parent_folder: str,
     state_path: str | None = None,
 ) -> None:
     """
@@ -20,8 +20,11 @@ async def main(
     :param rom_path: The path to the ROM file.
     :param state_path: Optional path to load a saved state from.
     """
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    folder = Path(parent_folder) / timestamp
+
     async with YellowLegacyEmulator(rom_path, state_path) as emulator:
-        agent_app = build_agent_application(Path(memory_dir), Path(backup_dir), emulator)
+        agent_app = build_agent_application(folder, emulator)
         try:
             await agent_app.arun()
         except Exception:  # noqa: BLE001
@@ -34,8 +37,7 @@ async def main(
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--rom-path", type=str, required=True)
-    parser.add_argument("--memory-dir", type=str, required=True)
-    parser.add_argument("--backup-dir", type=str, required=True)
+    parser.add_argument("--parent-folder", type=str, required=True)
     parser.add_argument("--state-path", type=str, required=False)
     args = parser.parse_args()
-    asyncio.run(main(args.rom_path, args.memory_dir, args.backup_dir, args.state_path))
+    asyncio.run(main(args.rom_path, args.parent_folder, args.state_path))
