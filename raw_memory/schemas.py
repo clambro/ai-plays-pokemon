9faab7e44fcef11bd5101a4cfa9eb7c2743bc54c
clambro <clambro.git@gmail.com>
2025-05-07 10:36:49 -0700

    Update schemas.py

--- raw_memory/schemas.py
+++ raw_memory/schemas.py
@@ -32,8 +32,9 @@ class RawMemory(BaseModel):
             f"Here are the raw, previous thoughts you have had prior to this point. The bracketed"
             f" number is the incremented iteration number at which you had the thought. Higher"
             f" numbers are more recent. Only the most recent {self.max_size} thoughts are displayed"
-            f" in this section.\n<raw_memory>\n"
+            f" in this section."
         )
+        out += "\n<raw_memory>\n"
         out += "\n".join([str(piece) for piece in reversed(self.pieces)])
         out += "\n</raw_memory>"
         return out
