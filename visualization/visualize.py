1ada0ab0305f0a929974086e497d8d1d0a689bc0
clambro <clambro.git@gmail.com>
2025-05-18 21:55:13 -0700

    Update visualize.py

--- visualization/visualize.py
+++ visualization/visualize.py
@@ -1,21 +1,15 @@
+from pathlib import Path
 from unittest.mock import MagicMock
-
-from junjo.graphviz.utils import graph_to_graphviz_image
 
 from agent.graph import build_agent_graph
 
 
 def main() -> None:
     """Visualize all the Burr graphs."""
-    dummy_emulator = MagicMock()  # We don't need a real emulator to visualize the graph.
-    names_and_graphs = [
-        ("Top Level Agent", build_agent_graph(dummy_emulator)),
-    ]
-
-    for name, graph in names_and_graphs:
-        # The file extension is added by graphviz.
-        filename = f"visualization/{name.lower().replace(' ', '_')}"
-        graph_to_graphviz_image(graph, filename)
+    graph = build_agent_graph(MagicMock())  # We don't need a real emulator to visualize the graph.
+    out_dir = Path("visualization/agent_graph/")
+    # PNG is preferable to SVG because we can directly visualize the diff in Git.
+    graph.export_graphviz_assets(out_dir=out_dir, fmt="png")
 
 
 if __name__ == "__main__":
