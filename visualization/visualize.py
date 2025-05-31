0b559ed791f25744d9a2fbbd0a6b5258a2c213a5
clambro <clambro.git@gmail.com>
2025-05-30 20:45:17 -0700

    Viz

--- visualization/visualize.py
+++ visualization/visualize.py
@@ -5,7 +5,7 @@ from agent.graph import build_agent_graph
 
 
 def main() -> None:
-    """Visualize all the Burr graphs."""
+    """Visualize all the Junjo graphs."""
     graph = build_agent_graph(MagicMock())  # We don't need a real emulator to visualize the graph.
     out_dir = Path("visualization/agent_graph/")
     # PNG is preferable to SVG because we can directly visualize the diff in Git.
