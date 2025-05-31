1f7b34a6064d3308537c9a2cc2fb3386bfbb8272
clambro <clambro.git@gmail.com>
2025-05-31 14:40:11 -0700

    Consolidate dummy nodes

--- agent/subflows/overworld_handler/graph.py
+++ agent/subflows/overworld_handler/graph.py
@@ -1,11 +1,11 @@
 from junjo import Edge, Graph
 
+from agent.nodes.dummy.node import DummyNode
 from agent.subflows.overworld_handler.conditions import ShouldCritique, ToolIs
 from agent.subflows.overworld_handler.nodes.critique.node import CritiqueNode
 from agent.subflows.overworld_handler.nodes.make_decision.node import MakeDecisionNode
 from agent.subflows.overworld_handler.nodes.navigate.node import NavigationNode
 from agent.subflows.overworld_handler.nodes.should_critique.node import ShouldCritiqueNode
-from agent.subflows.overworld_handler.nodes.sink.node import SinkNode
 from agent.subflows.overworld_handler.nodes.update_current_map.node import UpdateCurrentMapNode
 from agent.subflows.overworld_handler.nodes.update_onscreen_entities.node import (
     UpdateOnscreenEntitiesNode,
@@ -22,11 +22,12 @@ def build_overworld_handler_subflow_graph(emulator: YellowLegacyEmulator) -> Gra
     update_onscreen_entities = UpdateOnscreenEntitiesNode(emulator)
     decision_maker_overworld = MakeDecisionNode(emulator)
     navigation = NavigationNode(emulator)
-    sink = SinkNode()
+
+    dummy_sink = DummyNode()
 
     return Graph(
         source=update_current_map,
-        sink=sink,
+        sink=dummy_sink,
         edges=[
             Edge(
                 update_current_map,
@@ -57,12 +58,12 @@ def build_overworld_handler_subflow_graph(emulator: YellowLegacyEmulator) -> Gra
             ),
             Edge(
                 decision_maker_overworld,
-                sink,
+                dummy_sink,
                 ToolIs(None),
             ),
             Edge(
                 navigation,
-                sink,
+                dummy_sink,
             ),
         ],
     )
--- agent/subflows/text_handler/graph.py
+++ agent/subflows/text_handler/graph.py
@@ -1,9 +1,9 @@
 from junjo import Edge, Graph
 
+from agent.nodes.dummy.node import DummyNode
 from agent.subflows.text_handler.conditions import NeedsGenericHandling
 from agent.subflows.text_handler.nodes.handle_dialog_box.node import HandleDialogBoxNode
 from agent.subflows.text_handler.nodes.make_decision.node import MakeDecisionNode
-from agent.subflows.text_handler.nodes.sink.node import SinkNode
 from emulator.emulator import YellowLegacyEmulator
 
 
@@ -12,14 +12,15 @@ def build_text_handler_subflow_graph(emulator: YellowLegacyEmulator) -> Graph:
     # Trivial for now, but will be more complex in the future.
     handle_dialog_box = HandleDialogBoxNode(emulator)
     make_decision = MakeDecisionNode(emulator)
-    sink = SinkNode()
+
+    dummy_sink = DummyNode()
 
     return Graph(
         source=handle_dialog_box,
-        sink=sink,
+        sink=dummy_sink,
         edges=[
             Edge(handle_dialog_box, make_decision, NeedsGenericHandling(True)),
-            Edge(handle_dialog_box, sink, NeedsGenericHandling(False)),
-            Edge(make_decision, sink),
+            Edge(handle_dialog_box, dummy_sink, NeedsGenericHandling(False)),
+            Edge(make_decision, dummy_sink),
         ],
     )
