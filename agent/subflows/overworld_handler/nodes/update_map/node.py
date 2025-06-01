e1eeeba9c280a26e25ef32286cf332f9873ccae5
clambro <clambro.git@gmail.com>
2025-05-31 21:42:18 -0700

    Separate read/update logic

--- agent/subflows/overworld_handler/nodes/update_map/node.py
+++ agent/subflows/overworld_handler/nodes/update_map/node.py
@@ -1,15 +1,13 @@
 from junjo import Node
 from loguru import logger
 
-from agent.subflows.overworld_handler.nodes.update_onscreen_entities.service import (
-    UpdateOnscreenEntitiesService,
-)
+from agent.subflows.overworld_handler.nodes.update_map.service import UpdateMapService
 from agent.subflows.overworld_handler.state import OverworldHandlerStore
 from emulator.emulator import YellowLegacyEmulator
 
 
-class UpdateOnscreenEntitiesNode(Node[OverworldHandlerStore]):
-    """Update the onscreen entities based on the current game state."""
+class UpdateMapNode(Node[OverworldHandlerStore]):
+    """Update the current map and its entities based on the current game state."""
 
     def __init__(self, emulator: YellowLegacyEmulator) -> None:
         self.emulator = emulator
@@ -17,7 +15,7 @@ class UpdateOnscreenEntitiesNode(Node[OverworldHandlerStore]):
 
     async def service(self, store: OverworldHandlerStore) -> None:
         """The service for the node."""
-        logger.info("Updating the onscreen entities...")
+        logger.info("Updating the current map...")
 
         state = await store.get_state()
         if state.iteration is None:
@@ -25,12 +23,14 @@ class UpdateOnscreenEntitiesNode(Node[OverworldHandlerStore]):
         if state.current_map is None:
             raise ValueError("Current map is not set.")
 
-        service = UpdateOnscreenEntitiesService(
+        service = UpdateMapService(
             emulator=self.emulator,
             iteration=state.iteration,
             current_map=state.current_map,
             state_string_builder=state.to_prompt_string,
         )
-        await service.update_onscreen_entities()
+        current_map = await service.update_map()
+        await service.update_entities()
 
+        await store.set_current_map(current_map)
         await store.set_emulator_save_state_from_emulator(self.emulator)
