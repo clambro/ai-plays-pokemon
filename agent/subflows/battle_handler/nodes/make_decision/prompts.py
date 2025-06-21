09b1185e20944355f963a082d282b3ca3c36fb7a
clambro <clambro.git@gmail.com>
2025-06-21 13:54:09 -0700

    prompt tweaks

--- agent/subflows/battle_handler/nodes/make_decision/prompts.py
+++ agent/subflows/battle_handler/nodes/make_decision/prompts.py
@@ -1,6 +1,13 @@
 MAKE_DECISION_PROMPT = """
 You are in a pokemon battle. The screenshot provided above is the current game screen. You must decide which button to press to proceed with the battle.
 
+{state}
+
+Here is the game memory's representation of the onscreen text. The text you see below is exactly what the game is displaying on the screen, but the formatting may be somewhat messed up because it is not rendering images. Use it to help you understand the text on the screen, as well as the position of any cursors. This is a more reliable way to navigate menus than the screenshot.
+<onscreen_text>
+{text}
+</onscreen_text>
+
 The (case-sensitive) available buttons are:
 - a: The action button. Used to select the highlighted option or progress any on-screen text.
 - b: The back button. Used to go back to the previous screen or decline a yes/no question.
@@ -8,13 +15,6 @@ The (case-sensitive) available buttons are:
 - down: The down button. Used to move the cursor down one row.
 - left: The left button. Used to move the cursor left one column.
 - right: The right button. Used to move the cursor right one column.
-
-{state}
-
-Here is the game memory's representation of the onscreen text. The text you see below is exactly what the game is displaying on the screen, but the formatting may be somewhat messed up because it is not rendering images. Use it to help you understand the text on the screen, as well as the position of any cursors. This is a more reliable way to navigate menus than the screenshot.
-<onscreen_text>
-{text}
-</onscreen_text>
 
 Reflect on the information provided to you and respond in the format given below. The relevant keys are:
 - thoughts: Your thoughts on the current game state and which button to press. What is the state of the battle? Where is the cursor? Consider all the information presented to you above; reflect on it, and then respond with your thoughts. Limit this to one or two sentences.
--- agent/subflows/text_handler/nodes/make_decision/prompts.py
+++ agent/subflows/text_handler/nodes/make_decision/prompts.py
@@ -1,6 +1,14 @@
 DECISION_MAKER_TEXT_PROMPT = """
 There is text on the screen. The screenshot provided above is the current game screen. You must decide which button(s) to press to proceed with the game.
 
+{state}
+
+Here is the game memory's representation of the onscreen text. The text you see below is exactly what the game is displaying on the screen, but the formatting may be somewhat messed up because it is not rendering images. Use it to help you understand the text on the screen, as well as the position of any cursors. This is a more reliable way to navigate menus than the screenshot, but you must not ignore the screenshot altogether.
+<onscreen_text>
+{text}
+</onscreen_text>
+If you see garbled, nonsensical text in the onscreen text, it is because the game is rendering an image, which the memory stores as text. If this is the case, use the screenshot to help you better understand what is going on.
+
 The (case-sensitive) available buttons are:
 - a: The action button. Used to select a menu option or to continue the text.
 - b: The back button. Used to go back to the previous screen, close a menu, or decline a yes/no question.
@@ -9,14 +17,6 @@ The (case-sensitive) available buttons are:
 - down: Used to navigate the cursor down.
 - left: Used to navigate the cursor left.
 - right: Used to navigate the cursor right.
-
-{state}
-
-Here is the game memory's representation of the onscreen text. The text you see below is exactly what the game is displaying on the screen, but the formatting may be somewhat messed up because it is not rendering images. Use it to help you understand the text on the screen, as well as the position of any cursors. This is a more reliable way to navigate menus than the screenshot.
-<onscreen_text>
-{text}
-</onscreen_text>
-If you see garbled, nonsensical text in the onscreen text, it is because the game is rendering an image, which the memory stores as text. If this is the case, use the screenshot to help you better understand what is going on.
 
 Reflect on the information provided to you and respond in the format given below. The relevant keys are:
 - thoughts: Your thoughts on the current game state and which button(s) to press. Keep this to one or two sentences. It is important that you note any text that you are reading in your thoughts, otherwise you will lose access to it in subsequent turns.
