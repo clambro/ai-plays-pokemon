020889d125ffd0e3a74350a23733564285868ed6
clambro <clambro.git@gmail.com>
2025-06-22 19:36:02 -0700

    Update prompts.py

--- agent/subflows/text_handler/nodes/assign_name/prompts.py
+++ agent/subflows/text_handler/nodes/assign_name/prompts.py
@@ -3,15 +3,15 @@ You are at the name entry screen.
 
 {state}
 
-Which name would you like to enter? Respond using the following keys:
-- "thoughts": One or two sentences briefly considering what your current situation is and deciding on what an appropriate name would be and why. Make sure you actually say the name you would like to enter here in your thoughts.
-- "name": The name you would like to enter, as stated in your thoughts. Whatever you enter will be assigned as the name, exactly as you enter it.
-
 Naming rules:
 - The name must be uppercase.
 - The name must be at least one character long and at most ten characters long.
-  - The only exception to this is at the very beginning of the game when you are entering the names for yourself and your rival. These names are at most seven characters long.
+  - The only exception to this is in the intro screen (iteration number less than 15) when you are entering the names for yourself and your rival. These names are at most seven characters long.
 - The name can only contain the letters A-Z and spaces. Spaces count towards the character limit. Numbers are not allowed.
 - The name cannot start or end with a space.
 - Be creative. Don't use generic or default names. This is your chance to add some of your own personality to the game.
+
+Respond using the following keys:
+- "thoughts": One or two sentences briefly considering your situation and deciding on a good name. You must say the name you would like to enter here in your thoughts.
+- "name": The name you would like to enter, as stated in your thoughts. Whatever you enter will be assigned as the name, exactly as you enter it.
 """.strip()
