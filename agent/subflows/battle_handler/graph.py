10c944eb1688d0650d249ef97072c745b53e868c
clambro <clambro.git@gmail.com>
2025-06-27 10:47:21 -0700

    Update graph

--- agent/subflows/battle_handler/graph.py
+++ agent/subflows/battle_handler/graph.py
@@ -7,6 +7,7 @@ from agent.subflows.battle_handler.nodes.fight_tool.node import FightToolNode
 from agent.subflows.battle_handler.nodes.make_decision.node import MakeDecisionNode
 from agent.subflows.battle_handler.nodes.run_tool.node import RunToolNode
 from agent.subflows.battle_handler.nodes.switch_pokemon_tool.node import SwitchPokemonToolNode
+from agent.subflows.battle_handler.nodes.throw_ball_tool.node import ThrowBallToolNode
 from agent.subflows.battle_handler.schemas import (
     FightToolArgs,
     RunToolArgs,
@@ -22,6 +23,7 @@ def build_battle_handler_subflow_graph(emulator: YellowLegacyEmulator) -> Graph:
     make_decision = MakeDecisionNode(emulator)
     fight_tool = FightToolNode(emulator)
     switch_pokemon_tool = SwitchPokemonToolNode(emulator)
+    throw_ball_tool = ThrowBallToolNode(emulator)
     run_tool = RunToolNode(emulator)
     dummy_sink = DummyNode()
     return Graph(
@@ -31,10 +33,11 @@ def build_battle_handler_subflow_graph(emulator: YellowLegacyEmulator) -> Graph:
             Edge(determine_handler, make_decision, ToolArgsIs(None)),
             Edge(determine_handler, fight_tool, ToolArgsIs(FightToolArgs)),
             Edge(determine_handler, switch_pokemon_tool, ToolArgsIs(SwitchPokemonToolArgs)),
-            Edge(determine_handler, make_decision, ToolArgsIs(ThrowBallToolArgs)),
+            Edge(determine_handler, throw_ball_tool, ToolArgsIs(ThrowBallToolArgs)),
             Edge(determine_handler, run_tool, ToolArgsIs(RunToolArgs)),
             Edge(fight_tool, dummy_sink),
             Edge(switch_pokemon_tool, dummy_sink),
+            Edge(throw_ball_tool, dummy_sink),
             Edge(run_tool, dummy_sink),
             Edge(make_decision, dummy_sink),
         ],
