d0ad1c5bf139bd9392c82ab87ee3f03eb61b574e
clambro <clambro.git@gmail.com>
2025-07-09 22:34:50 -0700

    Make telemetry optional

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ async def main(
     *,
     mute_sound: bool = True,
     load_latest: bool = False,
+    track_telemetry: bool = False,
 ) -> None:
     """
     Get the emulator ticking on an async thread, and iteratively run the agent.
@@ -30,11 +31,13 @@ async def main(
     :param backup_folder: Optional path to load a saved state from.
     :param mute_sound: Whether to mute the sound.
     :param load_latest: Whether to load the latest backup.
+    :param track_telemetry: Whether to track telemetry.
     """
     if backup_folder and load_latest:
         raise ValueError("Cannot load latest backup and specify a backup folder at the same time.")
 
-    setup_telemetry()
+    if track_telemetry:
+        setup_telemetry()
 
     folder = await get_output_folder()
 
@@ -78,6 +81,7 @@ if __name__ == "__main__":
     parser.add_argument("--backup-folder", type=Path, required=False)
     parser.add_argument("--mute-sound", action="store_true")
     parser.add_argument("--load-latest", action="store_true")
+    parser.add_argument("--track-telemetry", action="store_true")
     args = parser.parse_args()
     asyncio.run(
         main(
@@ -85,5 +89,6 @@ if __name__ == "__main__":
             backup_folder=args.backup_folder,
             mute_sound=args.mute_sound,
             load_latest=args.load_latest,
+            track_telemetry=args.track_telemetry,
         )
     )
