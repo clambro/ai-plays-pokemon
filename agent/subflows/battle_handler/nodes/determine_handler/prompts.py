73a373339444d65987d8831403f27d1e6c0a1bb2
clambro <clambro.git@gmail.com>
2025-08-18 08:09:01 -0700

    Add cursor info to prompts (#71)
    * Update prompts.py
    
    * Update prompts.py
    
    * Update prompts.py
    
    * Update prompts.py

--- agent/subflows/battle_handler/nodes/determine_handler/prompts.py
+++ agent/subflows/battle_handler/nodes/determine_handler/prompts.py
@@ -3,10 +3,11 @@ You are in a Pokemon battle. The screenshot provided above is the current game s
 
 {state}
 
-Here is the game memory's representation of the onscreen text. The text you see below is exactly what the game is displaying on the screen, but the formatting may be somewhat messed up because it is not rendering images. Use it to help you understand what you see in the screenshot.
+Here is the game memory's representation of the onscreen text. The text you see below is exactly what the game is displaying on the screen, but the formatting may be somewhat messed up because it is not rendering images. Use it to help you understand the text on the screen, as well as the position of any cursors. If you see multiple cursors "▷" and "▶", you are probably in a nested menu. The active cursor is always "▶". This is a more reliable way to navigate menus than the screenshot, but keep the screenshot in mind as well.
 <onscreen_text>
 {text}
 </onscreen_text>
+If you see garbled, nonsensical text in the onscreen text, it is because the game is rendering an image, which the memory stores as text. If this is the case, use the screenshot to help you better understand what is going on.
 
 The available actions are:
 <actions>
@@ -15,6 +16,6 @@ The available actions are:
 Note that all of the above actions will use up your turn, and your opponent will have the opportunity to attack you. This means that if you switch Pokemon, your opponent will get a free attack against the Pokemon that you switch into.
 
 Reflect on the information provided to you and respond in the format given below. The relevant keys are:
-- thoughts: Your thoughts on the current game state and which action to take. Consider all the information presented to you above; reflect on it, and then respond with your thoughts. Limit this to one or two sentences.
+- thoughts: Your thoughts on the current game state and which action to take. What is the state of the battle? If present, where is the cursor? Consider all the information presented to you above; reflect on it, and then respond with your thoughts. Limit this to one or two sentences.
 - index: The index of the action to take (the number in the square brackets).
 """.strip()
--- agent/subflows/battle_handler/nodes/make_decision/prompts.py
+++ agent/subflows/battle_handler/nodes/make_decision/prompts.py
@@ -3,10 +3,11 @@ You are in a Pokemon battle. The screenshot provided above is the current game s
 
 {state}
 
-Here is the game memory's representation of the onscreen text. The text you see below is exactly what the game is displaying on the screen, but the formatting may be somewhat messed up because it is not rendering images. Images may be rendered as garbled text. Use it to help you understand the text on the screen, as well as the position of any cursors. This is a more reliable way to navigate menus than the screenshot, but you should pay attention to the screenshot as well.
+Here is the game memory's representation of the onscreen text. The text you see below is exactly what the game is displaying on the screen, but the formatting may be somewhat messed up because it is not rendering images. Use it to help you understand the text on the screen, as well as the position of any cursors. If you see multiple cursors "▷" and "▶", you are probably in a nested menu. The active cursor is always "▶". This is a more reliable way to navigate menus than the screenshot, but keep the screenshot in mind as well.
 <onscreen_text>
 {text}
 </onscreen_text>
+If you see garbled, nonsensical text in the onscreen text, it is because the game is rendering an image, which the memory stores as text. If this is the case, use the screenshot to help you better understand what is going on.
 
 The (case-sensitive) available buttons are:
 - a: The action button. Used to select the highlighted option or progress any on-screen text.
@@ -17,6 +18,6 @@ The (case-sensitive) available buttons are:
 - right: The right button. Used to move the cursor right one column.
 
 Reflect on the information provided to you and respond in the format given below. The relevant keys are:
-- thoughts: Your thoughts on the current game state and which button to press. What is the state of the battle? Where is the cursor? Consider all the information presented to you above; reflect on it, and then respond with your thoughts. Limit this to one or two sentences.
+- thoughts: Your thoughts on the current game state and which button to press. What is the state of the battle? If present, where is the cursor? Consider all the information presented to you above; reflect on it, and then respond with your thoughts. Limit this to one or two sentences.
 - buttons: The button(s) to press in sequence. Must be one or more of the available buttons above. You should generally only press one button at a time, but you may press multiple buttons in sequence to, say, move the cursor multiple times in a menu.
 """.strip()
