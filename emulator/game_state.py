7d1b9386c2adc22e1b3f2e4f00bd4d01f03225b3
clambro <clambro.git@gmail.com>
2025-08-18 09:09:16 -0700

    Update game_state.py

--- emulator/game_state.py
+++ emulator/game_state.py
@@ -260,6 +260,8 @@ class YellowLegacyGameState(BaseModel):
             else:
                 out += f"Type: {p.type1}\n"
             out += f"Level: {p.level}\n"
+            if p.level >= self.player.level_cap:
+                out += "This Pokemon is at the level cap and cannot be leveled up further.\n"
             out += f"HP: {p.hp} / {p.max_hp}\n"
             out += f"Status Ailment: {p.status}\n"
             out += "<moves>\n"
