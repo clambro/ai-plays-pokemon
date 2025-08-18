4eb8c648d3e8dfb9080e1369824da284dbadbfe8
clambro <clambro.git@gmail.com>
2025-08-18 08:03:01 -0700

    Update prompts.py

--- agent/subflows/battle_handler/nodes/determine_handler/prompts.py
+++ agent/subflows/battle_handler/nodes/determine_handler/prompts.py
@@ -3,10 +3,11 @@ You are in a Pokemon battle. The screenshot provided above is the current game s
 
 {state}
 
-Here is the game memory's representation of the onscreen text. The text you see below is exactly what the game is displaying on the screen, but the formatting may be somewhat messed up because it is not rendering images. Use it to help you understand what you see in the screenshot.
+Here is the game memory's representation of the onscreen text. The text you see below is exactly what the game is displaying on the screen, but the formatting may be somewhat messed up because it is not rendering images. Use it to help you understand the text on the screen, as well as the position of any cursors. If you see multiple cursors "▷" and "▶", you are probably in a nested menu. The active cursor is always "▶". This is a more reliable way to navigate menus than the screenshot, but keep the screenshot in mind as well.
 <onscreen_text>
 {text}
 </onscreen_text>
+If you see garbled, nonsensical text in the onscreen text, it is because the game is rendering an image, which the memory stores as text. If this is the case, use the screenshot to help you better understand what is going on.
 
 The available actions are:
 <actions>
